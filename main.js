9d6eba6e5f95adda4175a71ce5871f17f5bf3bd9
kosece <julianvarea14@gmail.com>
2022-03-15 22:44:14 -0300

    Added touch events and replaced repeating pieces of code with functions
    Added touchstart and touchend events for mobile compatibility.
    Replaced certain pieces of code with functions to made the code more readable and less bloated.

--- main.js
+++ main.js
@@ -49,10 +49,24 @@ const drumEffects = [
     }
 ]
 
-let fragmentoEffects = new DocumentFragment;
+const playSound = (audio) => {
+    audio.currentTime = 0; // "rewinds" the audio back to the beginning before playing it, allowing the user to play the same audio before it finishes playing.
+    audio.play();
+}
+
+const addAnimation = (div) => {
+    div.classList.add('pressed'); // Add 'pressed' status to the div that the key is linked to
+}
+
+const removeAnimation = (div) => {
+    div.classList.remove('pressed')
+}
+
+let fragmentoEffectDivs = new DocumentFragment;
 let fragmentoAudio = new DocumentFragment;
 
 for (effect of drumEffects) {
+    /* HTML ELEMENTS CREATION */
     let div = document.createElement("div");
     div.classList.add("effect");
     div.setAttribute('data-key', effect.keyListener)
@@ -60,31 +74,45 @@ for (effect of drumEffects) {
     let effectKey = document.createElement("h2");
     effectKey.textContent = effect.keyListener;
 
-    let effectTitle = document.createElement("h3");
-    effectTitle.textContent = effect.name;
+    let effectName = document.createElement("h3");
+    effectName.textContent = effect.name;
 
-    div.append(effectKey, effectTitle);
-    fragmentoEffects.appendChild(div);
+    div.append(effectKey, effectName);
 
     let audio = document.createElement("audio");
     audio.src = effect.src;
     audio.setAttribute('data-key', effect.keyListener)
-    fragmentoAudio.appendChild(audio)    
+    
+    /* Estos event listeners los creo directamente con los divs porque si los creo afuera tengo que 
+    usar un document.querySelectorAll para elegir todos los divs de efectos, y no me regresa nada al 
+    no ejecutarse DESPUES de que se creen los elementos por algun motivo */
+
+    div.addEventListener('touchstart', function(){
+        let audioToPlay = document.querySelector(`audio[data-key="${div.getAttribute('data-key')}"]`); // Select audio with the same data key as the touched div
+        playSound(audioToPlay);
+        addAnimation(div);
+    })
+
+    div.addEventListener('touchend', function() {
+        removeAnimation(div);
+    })
+
+    fragmentoAudio.appendChild(audio);
+    fragmentoEffectDivs.appendChild(div);
 }
 
 window.addEventListener('keypress', function(e) {
-    let audio = document.querySelector(`audio[data-key='${e.key}']`);
-    audio.currentTime = 0; // "rewinds" the audio back to the beginning before playing it, allowing the user to play the same audio before it finishes playing.
-    audio.play();
+    let audioToPlay = document.querySelector(`audio[data-key='${e.key}']`); // Select audio with data-key equal to the pressed key
+    playSound(audioToPlay); 
 
-    let divDelEfecto = document.querySelector(`div[data-key='${e.key}']`);
-    divDelEfecto.classList.add('pressed'); // Add 'pressed' status to the div that the key is linked to
+    let divToAnimate = document.querySelector(`div[data-key='${e.key}']`); // Select div with data-key equal to pressed div
+    addAnimation(divToAnimate);
 }) 
 
 window.addEventListener('keyup', function(e) {
-    document.querySelector(`.pressed[data-key="${e.key}"]`).classList.remove('pressed') // Remove pressed class from div whose key stopped being pressed.
+    let animatedDiv = document.querySelector(`.pressed[data-key="${e.key}"]`); // Select just the div linked with the key that stopped being pressed
+    removeAnimation(animatedDiv)
 })
 
-
-gridContainer.appendChild(fragmentoEffects)
+gridContainer.appendChild(fragmentoEffectDivs)
 body.appendChild(fragmentoAudio);
